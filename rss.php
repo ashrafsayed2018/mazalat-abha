be284b43b73d9740dbc36afd6c563b750b099a96
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-12-14 13:07:20 +0200

    initial

---
+++ rss.php
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom">
+  <channel>
+        <title>مظلات سواتر ابها خميس مشيط 0533723670</title>
+        <link>https://mzlatabha.com/</link>
+        <description>مظلات سواتر ابها خميس مشيط 0533723670</description>
+
